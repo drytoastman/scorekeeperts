bdd4537fb460d919abed54bf84edc79454a4331e
Brett Wilson <drytoastman@gmail.com>
2020-05-12 02:09:56 -0700

    simply markup and get payment dialog totals working

--- ui/src/Registration/components/PaymentDialog.vue
+++ ui/src/Registration/components/PaymentDialog.vue
@@ -1,78 +1,79 @@
 <template>
     <v-row justify="center">
-        <v-dialog :value="value" @input="$emit('input')" persistent max-width="400px">
-            <v-card>
-                <v-card-title>
-                    <span class="headline">Payments</span>
-                </v-card-title>
-                <v-card-text>
-                    <v-container>
-                        <v-form ref="form" lazy-validation>
-                            <div v-for="acct in paymentaccounts" :key="acct.accountid">
-                                <div v-for="e in accountEvents(acct.accountid)" :key="e.eventid" class='eventwrap'>
-                                    <h3>{{e.name}}</h3>
-                                    <div v-for="r in registered[e.eventid]" :key="r.eventid+r.carid" class='eventgrid'>
-                                        <CarLabel :car=cars[r.carid] fontsize="110%"></CarLabel>
-                                        <div v-if="hasPayments(r.eventid, r.carid)">
-                                            <span class='paidsum'>{{ paymentSum(r.eventid, r.carid)|dollars }}</span>
-                                        </div>
-                                        <v-select v-else :items="payitems" item-value="itemid">
-                                            <template v-slot:selection="d">
-                                                <span class='name'>{{ d.item.name }}</span> <span class='price'>{{ d.item.price|dollars }}</span>
-                                            </template>
-                                            <template v-slot:item="d">
-                                                <span class='name'>{{ d.item.name }}</span> <span class='price'>{{ d.item.price|dollars }}</span>
-                                            </template>
-                                        </v-select>
-                                    </div>
-                                </div>
-                                <v-btn>{{acct.type}} Payment</v-btn>
+        <v-dialog :value="value" @input="$emit('input')" persistent max-width="450px">
+            <v-container>
+                <v-btn class='close' icon @click="$emit('input')"><v-icon>{{close}}</v-icon></v-btn>
+
+                <v-card v-for="acct in paymentaccounts" :key="acct.accountid">
+                    <v-card-title>{{acct.name}}</v-card-title>
+                    <v-card-text v-for="e in accountEvents(acct.accountid)" :key="e.eventid" class='eventwrap'>
+                        <h3>{{e.name}}</h3>
+                        <div v-for="r in registered[e.eventid]" :key="r.eventid+r.carid" class='eventgrid'>
+                            <CarLabel :car=cars[r.carid] fontsize="110%"></CarLabel>
+                            <div v-if="hasPayments(r.eventid, r.carid)">
+                                <span class='paidsum'>{{ paymentSum(r.eventid, r.carid)|dollars }}</span>
                             </div>
-                        </v-form>
-                    </v-container>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="$emit('input')">Cancel</v-btn>
-                    <v-btn color="blue darken-1" text @click="update()">Pay</v-btn>
-                </v-card-actions>
-            </v-card>
+                            <v-select v-else :items="payitems(acct.accountid)" item-value="itemid" return-object hide-details solo dense
+                                @input="newpurchase(acct.accountid, r.eventid, r.carid, $event)"
+                            >
+                                <template v-slot:selection="d">
+                                    <span class='name'>{{ d.item.name }}</span> <span class='price'>{{ d.item.price|dollars }}</span>
+                                </template>
+                                <template v-slot:item="d">
+                                    <span class='name'>{{ d.item.name }}</span> <span class='price'>{{ d.item.price|dollars }}</span>
+                                </template>
+                            </v-select>
+                        </div>
+                    </v-card-text>
+
+                    <v-card-actions class='eventgrid'>
+                        <div></div>
+                        <v-btn color=secondary :disabled="!(total[acct.accountid]>0)">{{acct.type}} Payment {{total[acct.accountid]|dollars}}</v-btn>
+                    </v-card-actions>
+                </v-card>
+
+            </v-container>
         </v-dialog>
     </v-row>
 </template>
 
 <script>
+import Vue from 'vue'
 import _ from 'lodash'
 import { mapState } from 'vuex'
 import CarLabel from '../../components/CarLabel'
 import { EventWrap } from '@common/lib'
+import { mdiCloseBox } from '@mdi/js'
+
 
 export default {
     components: {
         CarLabel
     },
     props: {
-        value: Boolean,
-        event: Object,
-        reg: Array
+        value: Boolean
     },
     filters: {
         dollars: function(v) {
-            if (typeof v !== 'number') return typeof v
+            if (typeof v !== 'number') return ''
             return `$${(v / 100).toFixed(2)}`
         }
     },
     data() {
         return {
-            checks: {}
+            close: mdiCloseBox,
+            purchase: {},
+            total: {}
         }
     },
     computed: {
         ...mapState(['events', 'registered', 'cars', 'payments', 'paymentitems', 'paymentaccounts']),
-        orderedOpenEvents() { return _.orderBy(this.events, ['date']).filter(e => new EventWrap(e).isOpen()) },
-        payitems() { return [{ itemid: null, name: 'No payment' }, ...this.paymentitems] }
+        orderedOpenEvents() { return _.orderBy(this.events, ['date']).filter(e => new EventWrap(e).isOpen()) }
     },
     methods: {
+        payitems(accountid) {
+            return [{ itemid: null, name: '' }, ...this.paymentitems.filter(i => i.accountid === accountid)]
+        },
         accountEvents(accountid) {
             return this.orderedOpenEvents.filter(e => e.accountid === accountid)
         },
@@ -82,13 +83,12 @@ export default {
         paymentSum(eventid, carid) {
             return _.sumBy(this.payments[eventid][carid], 'amount')
         },
-        itemText(item) {
-            return `${item.name} - ${item.price}`
-        },
-        update() {
-            // Create new reg objects and send request
-            // this.$store.dispatch('setdata', {
-            this.$emit('input')
+        newpurchase(accountid, eventid, carid, value) {
+            if (!(accountid in this.purchase)) {
+                this.purchase[accountid] = {}
+            }
+            this.purchase[accountid][eventid + carid] = value
+            Vue.set(this.total, accountid, _(this.purchase[accountid]).values().sumBy('price'))
         }
     },
     watch: {
@@ -104,23 +104,32 @@ export default {
 <style scoped>
 .eventgrid {
     display: grid;
-    grid-template-columns: 1fr 1fr;
     align-items: center;
-}
-.v-input {
-    padding: 0;
-}
-.price {
-    margin-left: 10px;
+    grid-template-columns: 3fr 4fr;
+    grid-template-rows: 50px;
 }
 .paidsum {
     font-style: italic;
     color: gray;
 }
+
 h3 {
-    margin-top: 1rem;
+    margin-top: 0.5rem;
+    border-bottom: 1px solid #CCC;
 }
-.eventwrap:first-child h3 {
-    margin-top: 0;
+.v-card__text {
+    margin-bottom: 1rem;
+}
+
+.container {
+    background: #DDD;
+    padding: 1px 10px;
+}
+.close {
+    display: block;
+    margin-left: auto;
+}
+.v-card {
+    margin-bottom: 1rem;
 }
 </style>
