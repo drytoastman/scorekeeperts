b6aafcb002b2caf15fbb48bbca2ea1e91228d60f
Brett Wilson <drytoastman@gmail.com>
2020-05-29 14:27:37 -0700

    add inuse indicator

--- ui/src/components/CarCard.vue
+++ ui/src/components/CarCard.vue
@@ -3,31 +3,38 @@
         <v-card-title>
             <CarLabel :car=car></CarLabel>
         </v-card-title>
-        <v-card-actions>
-            <v-btn text @click="$emit('editcar', car)">Edit</v-btn>
-            <v-btn text @click="$emit('deletecar', car)">Delete</v-btn>
+        <v-card-actions v-on="on">
+            <v-btn text @click="$emit('editcar', car)"   :disabled="inreg">Edit</v-btn>
+            <v-btn text @click="$emit('deletecar', car)" :disabled="inreg">Delete</v-btn>
+            <div class='inuse' v-if="inreg">In Use</div>
         </v-card-actions>
     </v-card>
 </template>
 
 <script>
+import _ from 'lodash'
 import { mapState } from 'vuex'
 import CarLabel from './CarLabel'
 export default {
     components: { CarLabel },
     props: { car: Object },
     computed: {
-        ...mapState(['busyCars']),
-        busy() { return this.busyCars[this.car.carid] }
+        ...mapState(['busyCars', 'registered']),
+        busy() { return this.busyCars[this.car.carid] },
+        inreg() { return Boolean(_(this.registered).flatMap().find({ carid: this.car.carid })) }
     }
 }
 </script>
 
 <style scoped>
-    .v-card {
-        height: 100%;
-    }
-    .v-card--disabled > :not(.v-card__progress) {
-        opacity: 0.3;
-    }
+.v-card {
+    height: 100%;
+}
+.v-card--disabled > :not(.v-card__progress) {
+    opacity: 0.3;
+}
+.inuse {
+    margin-left: 1rem;
+    color: #A33;
+}
 </style>
