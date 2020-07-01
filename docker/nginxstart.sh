cf5dbe8e5b003d97639950dd0b349bf336315d94
Brett Wilson <drytoastman@gmail.com>
2020-06-30 23:31:16 -0700

    change file name so we can get mime type, add compression

--- docker/nginxstart.sh
+++ docker/nginxstart.sh
@@ -2,9 +2,9 @@
 set -e
 if [ -z "${MAIN_SERVER}" ]; then
     echo "Running in Local mode"
-    cp /etc/nginx/nginx.local.conf /etc/nginx/nginx.conf
+    cp /etc/nginx/servers.local.conf /etc/nginx/servers.conf
 else
     echo "Running in SSL mode"
-    cp /etc/nginx/nginx.ssl.conf /etc/nginx/nginx.conf
+    cp /etc/nginx/servers.ssl.conf /etc/nginx/servers.conf
 fi
 nginx -g 'daemon off;'
