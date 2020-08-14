4ef3be612de505ea1b2816f32149af004af935d5
Brett Wilson <drytoastman@gmail.com>
2020-08-13 23:59:30 -0700

    logs download

--- server/src/controllers/logs.ts
+++ server/src/controllers/logs.ts
@@ -1,21 +1,56 @@
 import _ from 'lodash'
+import AdmZip from 'adm-zip'
 import { Request, Response } from 'express'
 import fs from 'fs'
 import readLastLines from 'read-last-lines'
 import util from 'util'
 
-import { checkAuth } from './apiauth'
-
 const readdirAsync = util.promisify(fs.readdir)
+const readfileAsync = util.promisify(fs.readFile)
 
 export async function logs(req: Request, res: Response) {
     if (!req.auth.hasAdminAuth()) {
-        // return res.status(401).json({ error: 'not authenticated', authtype: 'admin' })
+        return res.status(401).json({ error: 'not authenticated', authtype: 'admin' })
     }
 
-    const ret = {}
-    for (const file of await readdirAsync('/var/log')) {
-        ret[file] = await readLastLines.read(`/var/log/${file}`, 10)
+    const lines = req.query.lines || 10
+    const download = 'separate' in req.query
+    const interleave = 'interleave' in req.query
+
+    if (!download && !interleave) {
+        const ret = {}
+        for (const file of await readdirAsync('/var/log')) {
+            ret[file] = await readLastLines.read(`/var/log/${file}`, lines)
+        }
+        return res.json(ret)
     }
-    return res.json(ret)
+
+    if (download) {
+        const ret = {}
+        const zip = new AdmZip()
+        for (const file of await readdirAsync('/var/log')) {
+            zip.addLocalFile(`/var/log/${file}`)
+        }
+
+        res.setHeader('Content-disposition', 'attachment; filename=logs.zip')
+        res.contentType('application/zip')
+        return res.send(zip.toBuffer())
+    }
+
+    if (interleave) {
+        const ret:string[] = []
+
+        for (const file of await readdirAsync('/var/log')) {
+            const data = await readfileAsync(`/var/log/${file}`, 'utf-8')
+            const lines = data.split('\n')
+            for (const line of lines) {
+                ret.push(line.substr(0, 19).replace(/\//g, '-') + ` ${file} `.padEnd(15, ' ') + line.substr(20))
+            }
+        }
+
+        ret.sort()
+        res.setHeader('Content-disposition', 'attachment; filename=interleaved.log')
+        res.contentType('text/plain')
+        return res.send(ret.join('\n'))
+    }
 }
--- ui/src/Admin/views/serieslogs.vue
+++ ui/src/Admin/views/serieslogs.vue
@@ -1,21 +1,71 @@
 <template>
     <div class='outer'>
-        Log files
+        <v-slider v-model="lines" class="align-center" :max="100" :min="10" hide-details>
+            <template v-slot:append><span style='white-space:nowrap'>{{lines}} Lines</span></template>
+        </v-slider>
+        <div class='controls'>
+            <v-btn @click='reload'>Reload</v-btn>
+            <a :href="links" download>Download All Separate</a>
+            <a :href="linki" download>Download All Interleaved</a>
+        </div>
+
+        <div class='log' v-for="name in Object.keys(logs).sort()" :key="name">
+            <div class='name'>{{name}}</div>
+            <div class='text' v-for="line in logs[name].split('\r\n')" :key="line[-1]">{{line}}</div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { API2ROOT } from '@/store/state'
+
 export default {
     name: 'Summary',
     props: {
         series: String
+    },
+    data() {
+        return {
+            logs: {},
+            lines: 20,
+            links: API2ROOT + '/logs?separate',
+            linki: API2ROOT + '/logs?interleave'
+        }
+    },
+    methods: {
+        reload() {
+            axios.get(API2ROOT + '/logs', { params: { lines: this.lines }, withCredentials: true }).then(resp => {
+                this.logs = resp.data
+            }).catch(error => {
+                console.log(error)
+            })
+        }
+    },
+    mounted() {
+        this.reload()
     }
 }
 </script>
 
 <style scoped>
 .outer {
-    margin: 3rem;
+    width: 90vw;
+}
+.controls {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+    justify-items: center;
+    column-gap: 1rem;
+}
+.name {
     font-weight: bold;
+    margin-top: 1rem;
+    border-bottom: 1px solid lightgray;
+}
+.text {
+    font-family: 'Courier New', Courier, monospace;
+    white-space: nowrap;
 }
 </style>
