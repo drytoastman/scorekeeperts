7fe2769f7fc81c81a6af515ecfe509c31a719ea4
Brett Wilson <drytoastman@gmail.com>
2020-08-21 00:57:07 -0700

    extract selection table into its own component

--- ui/src/Admin/views/drivereditor.vue
+++ ui/src/Admin/views/drivereditor.vue
@@ -2,18 +2,7 @@
     <div class='outer'>
         <div v-if="sizeWarning" class='sizewarning'>The display width is less then the recommended minimum 950px for this tool</div>
         <div class='twocol'>
-            <div class='searchbox'>
-                <v-text-field v-model="search" dense placeholder="search" class='searchfield'></v-text-field>
-                <div class='namelist'>
-                    <table class='nametable'>
-                        <tr v-for="d in searched" :key="d.driverid" @click="trclick($event, d.driverid)">
-                            <td>{{d.firstname|lenlimit(12)}}</td>
-                            <td>{{d.lastname|lenlimit(16)}}</td>
-                            <td>{{d.email|lenlimit(20)}}</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+            <DriverSearchSelect @add="addid" @del="delid"></DriverSearchSelect>
             <div class='displaybox'>
                 <div v-for="driver in selectedDrivers" :key="driver.driverid" class='driverbox'>
                     <div class='smallbuttons'>
@@ -26,7 +15,9 @@
                     <div class='serieswrapper' v-for="(cars, series) in drivercars(driver.driverid)" :key="series">
                         <div class='series'>{{series}}</div>
                         <div class='carbox' v-for="car in cars" :key="car.carid">
-                            <CarLabel :car=car></CarLabel>
+                            <CarLabel :car=car>
+                                <span class='carid'>{{car.carid}}</span>
+                            </CarLabel>
                             <div class='smallbuttons'>
                                 <template v-if="car.eventsrun || car.eventsreg">
                                     <v-btn color=secondary outlined small>Edit <span class='use'>*In Use</span></v-btn>
@@ -49,36 +40,26 @@
 </template>
 
 <script>
-import isEmpty from 'lodash/isEmpty'
 import { mapState } from 'vuex'
 import Vue from 'vue'
 import Driver from '../../components/Driver'
 import CarLabel from '../../components/CarLabel'
+import DriverSearchSelect from '../components/DriverSearchSelect'
 
 export default {
     name: 'DriverEditor',
     components: {
         Driver,
-        CarLabel
+        CarLabel,
+        DriverSearchSelect
     },
     data() {
         return {
-            search: '',
             selected: {}
         }
     },
     computed: {
-        ...mapState(['drivers', 'driverbrief', 'cars']),
-        searched() {
-            if (!this.search) return this.driverbrief
-            const reg = this.search.split(' ').map(t => new RegExp(t, 'i'))
-            return this.driverbrief.filter(d => {
-                for (const r of reg) {
-                    if (!(r.test(d.firstname) || r.test(d.lastname) || r.test(d.email))) return false
-                }
-                return true
-            })
-        },
+        ...mapState(['drivers', 'cars']),
         selectedDrivers() {
             return Object.keys(this.selected).map(did => this.drivers[did]).filter(v => v)
         },
@@ -87,28 +68,12 @@ export default {
         }
     },
     methods: {
-        trclick(event, driverid) {
-            const trelement = event.target.parentElement // clicks return td
-            if (driverid in this.selected) {
-                this.unselect(driverid)
-            } else {
-                if (event.getModifierState('Shift')) {          // select all between?  TBD, FINISH ME
-                } else if (event.getModifierState('Control')) { // individual select of new value
-                    this.select(driverid, trelement)
-                } else {                                        // unselect others
-                    Object.keys(this.selected).forEach(did => this.unselect(did))
-                    this.select(driverid, trelement)
-                }
-            }
-        },
-        unselect(driverid) {
-            this.selected[driverid].classList.remove('selected')
-            Vue.delete(this.selected, driverid)
-        },
-        select(driverid, trelement) {
-            trelement.classList.add('selected')
-            Vue.set(this.selected, driverid, trelement)
+        addid(driverid) {
+            Vue.set(this.selected, driverid, 1)
             this.$store.dispatch('ensureEditorInfo', [driverid])
+        },
+        delid(driverid) {
+            Vue.delete(this.selected, driverid)
         },
         drivercars(driverid) {
             const ret = {}
@@ -120,11 +85,6 @@ export default {
         },
         driveruse(driverid) {
             return Object.values(this.cars).filter(c => c.driverid === driverid && (c.eventsrun || c.eventsreg)).length > 0
-        }
-    },
-    mounted() {
-        if (isEmpty(this.driverbrief)) {
-            this.$store.dispatch('getdata', { items: 'driverbrief' })
         }
     }
 }
@@ -139,37 +99,6 @@ export default {
 .twocol {
     display: flex;
     column-gap: 1rem;
-}
-.searchbox {
-    display: flex;
-    flex-direction: column;
-    .searchfield {
-        flex-grow: 0;
-    }
-    .namelist {
-        flex-grow: 1;
-        height: 80vh;
-        width: 45vw;
-        overflow: scroll;
-    }
-    .nametable {
-        border-collapse: collapse;
-        width: 100%;
-
-        tr.selected {
-            background-color: rgba(0,150,0,0.25) !important;
-        }
-        tr:nth-of-type(2n+1) {
-            background-color: rgba(0,0,0,0.05);
-        }
-        td {
-            padding: 2px 10px 2px 3px;
-            border: 1px solid lightgray;
-            font-size: 80%;
-            cursor: pointer;
-            user-select: none;
-        }
-    }
 }
 .displaybox {
     .use {
@@ -217,6 +146,10 @@ export default {
             display: grid;
             grid-template-columns: auto 10rem;
             align-items: center;
+            .carid {
+                font-size: 70%;
+                color: grey;
+            }
         }
         @media (max-width: 800px) {
             .carbox {
