c84e36b0b80da7215d65b216bef565eefbfeccf9
Brett Wilson <drytoastman@gmail.com>
2020-08-27 00:48:57 -0700

    fix expiry check and tz display

--- server/src/util/statelessdata.ts
+++ server/src/util/statelessdata.ts
@@ -1,5 +1,5 @@
 import KeyGrip from 'keygrip'
-import moment from 'moment'
+import moment from 'moment-timezone'
 import { base64encode, base64decode } from 'nodejs-base64'
 
 /* Essentially copying what the python itsdangerous library did */
@@ -16,9 +16,10 @@ export function unwrapObj(keygrip: KeyGrip, encoded: string, sig: string): any {
     if (!keygrip.verify(encoded, sig)) {
         throw Error('Invalid token signature')
     }
-    const obj = JSON.parse(base64decode(encoded))
-    if (obj.expires > moment()) {
-        throw Error(`This token expired ${obj.expires}`)
+    const obj     = JSON.parse(base64decode(encoded))
+    const expires = moment(obj.expires).tz(process.env.TZ || 'America/Los_Angeles')
+    if (expires < moment()) {
+        throw Error(`This token expired ${expires.format('llll z')}`)
     }
     return obj
 }
