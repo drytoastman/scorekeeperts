5dc252f98f3cd028f55b90b7b1b33f9d04420082
Brett Wilson <drytoastman@gmail.com>
2020-12-14 03:13:28 -0800

    necessary udates for docker build with new common dir

---
+++ common/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+    },
+}
