cf0262aa30effb17b337fbbaffb0807c86ae21ce
Brett Wilson <drytoastman@gmail.com>
2020-12-18 01:38:32 -0800

    compile to commonjs for shared modules, couldn't get babel-jest to work

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -2,10 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     moduleNameMapper: {
-        '@/(.*)$': '<rootDir>/src/$1',
-        'sctypes/lib/(.*)$': '<rootDir>/../common/types/$1',
-        '@scdb': '<rootDir>/../common/db',
-        '@scdb/(.*)$': '<rootDir>/../common/db/$1'
+        '@/(.*)$': '<rootDir>/src/$1'
     },
     testPathIgnorePatterns: [
         'helpers.ts',
